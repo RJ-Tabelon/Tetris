496aa0d56d4c22b9686cd4e98432568e24f5c364
RJ Tabelon <tabelonrj@gmail.com>
2025-08-03 07:33:55 -0400

    Designed user interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include "game.h"
+#include "colors.h"
 
 double lastUpdateTime = 0;
 
@@ -13,19 +14,27 @@ bool EventTriggered(double interval) {
 }
 
 int main() {
-    Color darkBlue = {44, 44, 127, 255}; 
-    InitWindow(300, 600, "Tetris"); // initialize game window (width, height, title)
+    InitWindow(500, 620, "Tetris"); // initialize game window (width, height, title)
     SetTargetFPS(60); // 60 frames per second
+
+    Font font = LoadFontEx("Font/ARCADECLASSIC.TTF", 64, 0, 0);
 
     Game game = Game();
 
     while(WindowShouldClose() == false) {
         game.HandleInput();
-        if (EventTriggered(0.02)) {
+        if (EventTriggered(0.3)) {
             game.MoveBlockDown();
         }
         BeginDrawing();
-        ClearBackground(darkBlue);
+        ClearBackground(lightPurple);
+        DrawTextEx(font, "Score", {362, 15}, 30, 2, WHITE);
+        DrawTextEx(font, "Next", {372, 175}, 30, 2, WHITE);
+        if (game.gameOver) {
+            DrawTextEx(font, "Game Over", {332, 450}, 30, 2, WHITE);
+        }
+        DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lighterPurple);
+        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lighterPurple);
         game.Draw();
         EndDrawing();
     }
