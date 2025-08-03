041f916598c237a3c0a31a0da7137ecf82d44ded
RJ Tabelon <tabelonrj@gmail.com>
2025-08-03 07:34:49 -0400

    Changed colors

--- src/colors.cpp
+++ src/colors.cpp
@@ -1,6 +1,6 @@
 #include "colors.h"
 
-const Color darkGrey = {26, 31, 40, 255};
+const Color darkPurple = {31, 31, 88, 255};
 const Color green = {47, 230, 23, 255};
 const Color red = {232, 18, 18, 255};
 const Color orange = {226, 116, 17, 255};
@@ -8,7 +8,9 @@ const Color yellow = {237, 234, 4, 255};
 const Color purple = {166, 0, 247, 255};
 const Color cyan = {21, 204, 209, 255};
 const Color blue = {13, 64, 216, 255};
+const Color lightPurple = {89, 79, 126, 255};
+const Color lighterPurple = {142, 130, 177, 255};
 
 std::vector<Color> GetCellColors() {
-    return {darkGrey, green, red, orange, yellow, purple, cyan, blue};
+    return {darkPurple, green, red, orange, yellow, purple, cyan, blue};
 }
